938115a8ecfc30044f5798bfe65e5c599dc9c648
Sudhansu Sekhar Sahoo <sudhansu.sahoo@neoastra.com>
2023-06-23 15:13:37 +0530

    Created a custom hook useFetch

---
+++ src/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+            .then(response => {
+                console.log(response)
+                if(!response.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
+                return response.json()
+            })
+            .then(data => {
+                console.log(data)
+                setData(data);
+                setIsPending(false);
+                setError(null)
+            })
+            .catch(err => {
+                console.log(err.message)
+                setError(err.message)
+                setIsPending(false)
+            })
+        }, 1000);
+        
+    }, [url])
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
