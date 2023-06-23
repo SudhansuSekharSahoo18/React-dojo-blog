ba82c8307ed77a8303ed1c1611b48ae39e2f02e3
Sudhansu Sekhar Sahoo <sudhansu.sahoo@neoastra.com>
2023-06-24 01:12:33 +0530

    Added blog detail page and routing

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const person = {name: 'yoshi', age: 30 }
-
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
           <Switch>
+            
+            <Route exact path="/">
+              <Home />
+            </Route>
+
             <Route path="/create">
               <Create />
             </Route>
-            <Route path="/">
-              <Home />
+            
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
             
           </Switch>
